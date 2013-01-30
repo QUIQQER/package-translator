715e697f37ab8d4451e9b5bfb0d946f5892eb208
Henning Leutz <leutz@pcsg.de>
2013-01-30 13:18:41 +0100

    translator begin

---
+++ bin/Translator.js
@@ -0,0 +1,18 @@
+/**
+ * Comment here
+ *
+ * @author www.pcsg.de (Henning Leutz)
+ *
+ * @module
+ * @package
+ * @namespace
+ */
+
+define('package/quiqqer/translator/bin/Translator', [
+
+    "controls/Control"
+
+], function(QUI_Control)
+{
+
+});
