8c0eca3f967407f9200042d6e151eca53efca775
Jan Wennrich <jan.wennrich@pcsg.de>
2019-01-25 13:45:58 +0100

    refactor: added more details to translation var exception

--- ajax/update.php
+++ ajax/update.php
@@ -29,7 +29,11 @@ QUI::$Ajax->registerFunction(
             throw new QUI\Exception(
                 QUI::getLocale()->get(
                     'quiqqer/translator',
-                    'exception.translation.not.found.update'
+                    'exception.translation.var.not.found.update',
+                    [
+                        'var' => $data['var'],
+                        'group' => $groups
+                    ]
                 ),
                 404
             );
